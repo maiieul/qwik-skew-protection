0f337d6cdb95b74e108119880b3d2c0c2cc37b90
maiieul <maieul.chevalier@gmail.com>
2024-08-06 09:02:38 +0200

    add Form comp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,38 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { Form, routeAction$, type DocumentHead } from "@builder.io/qwik-city";
+
+export const useAddUser = routeAction$(async (data) => {
+  // This will only run on the server when the user submits the form (or when the action is called programmatically)
+
+  return {
+    success: true,
+    firstName: data.firstName,
+    lastName: data.lastName,
+  };
+});
 
 export default component$(() => {
+  const action = useAddUser();
+
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1>Version 1</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </div>
+      <br />
+
+      <Form action={action}>
+        <input name="firstName" />
+        <input name="lastName" />
+        <button type="submit">Add user</button>
+      </Form>
+      {action.value?.success && (
+        // When the action is done successfully, the `action.value` property will contain the return value of the action
+        <p>Hi {action.value.firstName + " " + action.value.lastName}</p>
+      )}
     </>
   );
 });
