2f745b1e084d234df168f2a044668ffa47cea5b1
maiieul <maieul.chevalier@gmail.com>
2024-08-07 09:10:04 +0200

    remove Cache-Control

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,16 +1,16 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
+export const onGet: RequestHandler = async () => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
-  cacheControl({
-    public: true,
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 60 * 60 * 24,
-  });
+  // cacheControl({
+  //   public: true,
+  //   // Always serve a cached response by default, up to a week stale
+  //   staleWhileRevalidate: 60 * 60 * 24 * 7,
+  //   // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+  //   maxAge: 60 * 60 * 24,
+  // });
 };
 
 export default component$(() => {
